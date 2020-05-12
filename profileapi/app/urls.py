322cbb517c4f8ac737baa410a299152ba33acc5a
manu pandey <pmrityunjay8888@gmail.com>
2020-05-12 21:28:39 +0530

    added viewset functionality to hello world api

--- profileapi/app/urls.py
+++ profileapi/app/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import hello_world
+from django.urls import path,include
+from .views import hello_world,HelloViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router=DefaultRouter()
+router.register('hello-viewset',HelloViewSet,basename='hello-viewset')
+
 
 urlpatterns=[
-    path('',hello_world.as_view())
+    path('',hello_world.as_view()),
+    path('',include(router.urls))
 ]
