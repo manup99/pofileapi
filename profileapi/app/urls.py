31c32ee760f8a66d7da77d8a5808146de49e1ef5
manu pandey <pmrityunjay8888@gmail.com>
2020-05-12 19:35:45 +0530

    changes in url.py file

--- profileapi/app/urls.py
+++ profileapi/app/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
+from .views import hello_world
 
+urlpatterns=[
+    path('',hello_world.as_view())
+]
--- profileapi/app/views.py
+++ profileapi/app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class hello_world(APIView):
+    def get(self,request,format=None):
+        api_view=[
+            'Just checking our api',
+            'You can also check yourself',
+        ]
+        return Response({'Message':'Hello','api_view':api_view})
