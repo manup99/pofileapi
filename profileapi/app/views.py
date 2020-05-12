ab7ae6f91f224ae20d8cbf299e818349472669bc
manu pandey <pmrityunjay8888@gmail.com>
2020-05-12 19:55:48 +0530

    added put patch post delete method to our view

--- profileapi/app/views.py
+++ profileapi/app/views.py
@@ -1,10 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from .serializers import HelloSerializer
+from rest_framework import status
 class hello_world(APIView):
+    serializer_class=HelloSerializer
     def get(self,request,format=None):
         api_view=[
             'Just checking our api',
             'You can also check yourself',
         ]
         return Response({'Message':'Hello','api_view':api_view})
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,pk=None):
+        return Response({'method':'PUT'})
+    def patch(self,request,pk=None):
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        return Response({'method':'DELETE'})
