86b752c4d113e850c90e3bddd0b80171e9dae886
manu pandey <pmrityunjay8888@gmail.com>
2020-05-15 13:31:30 +0530

    added Feed model in app

--- profileapi/app/models.py
+++ profileapi/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 # Create your models here.
 
 
@@ -44,3 +45,14 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         return self.name
     def __str__(self):
         return self.email
+
+
+class Feed(models.Model):
+    """Profile status update"""
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    status_text=models.CharField(max_length=255)
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.status_text
